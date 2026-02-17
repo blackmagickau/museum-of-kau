7bc01596dd113538c8b9c4f5f9ce2bda1b0fb1ff
Kaushik <kyadunath@gmail.com>
2026-02-17 20:25:49 +0800

    analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -110,6 +110,8 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
           </div>
         </footer>
       )}
+      {/* Vercel Analytics */}
+<Analytics />
     </div>
   );
 };
