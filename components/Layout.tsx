5b4415cd6e4659df987417d6f90d41dff8498f5a
Kaushik <kyadunath@gmail.com>
2026-02-17 20:20:08 +0800

    changed to react analytics for vercel

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 import { useLocation, Link } from 'react-router-dom';
 import { CURATOR } from '../constants';
 
